4a0c1196d7aab2f74503c0e0459532e5f5394407
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-23 13:14:17 -0300

    Adding head data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { Fragment } from 'react';
+import Head from 'next/head';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import Hero from '../components/home-page/Hero';
 import { getFeaturedPosts } from '../helpers/posts-util';
@@ -6,6 +7,13 @@ import { getFeaturedPosts } from '../helpers/posts-util';
 function HomePage(props) {
 	return (
 		<Fragment>
+			<Head>
+				<title>Cyn's Blog</title>
+				<meta
+					name="description"
+					content="A small blog made in Next, React and MongoDB"
+				/>
+			</Head>
 			<Hero />
 			<FeaturedPosts posts={props.posts} />
 		</Fragment>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,8 +1,18 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
 import AllPosts from '../../components/posts/all-posts';
 import { getAllPosts } from '../../helpers/posts-util';
 
 function AllPostsPage(props) {
-	return <AllPosts posts={props.posts} />;
+	return (
+		<Fragment>
+			<Head>
+				<title>All Posts - Cyn's Blog</title>
+				<meta name="description" content="See all posts in Cyn's Blog" />
+			</Head>
+			<AllPosts posts={props.posts} />
+		</Fragment>
+	);
 }
 
 //pre-generating props -> all posts
